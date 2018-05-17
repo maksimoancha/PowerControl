b57536e19bd59790220ddf12f35587fa36298154
Maksim Oancha <maksimoancha12@gmail.com>
2018-05-17 23:10:09 +0300

    Add project files.

---
+++ PowerControl/MainForm.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace PowerControl
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.powerOFFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sleepToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hybernateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logOFFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lockUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Power Control";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.powerOFFToolStripMenuItem,
+            this.sleepToolStripMenuItem,
+            this.hybernateToolStripMenuItem,
+            this.logOFFToolStripMenuItem,
+            this.lockUserToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(132, 136);
+            // 
+            // powerOFFToolStripMenuItem
+            // 
+            this.powerOFFToolStripMenuItem.Name = "powerOFFToolStripMenuItem";
+            this.powerOFFToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.powerOFFToolStripMenuItem.Text = "Power OFF";
+            this.powerOFFToolStripMenuItem.Click += new System.EventHandler(this.powerOFFToolStripMenuItem_Click);
+            // 
+            // sleepToolStripMenuItem
+            // 
+            this.sleepToolStripMenuItem.Name = "sleepToolStripMenuItem";
+            this.sleepToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.sleepToolStripMenuItem.Text = "Sleep";
+            this.sleepToolStripMenuItem.Click += new System.EventHandler(this.sleepToolStripMenuItem_Click);
+            // 
+            // hybernateToolStripMenuItem
+            // 
+            this.hybernateToolStripMenuItem.Name = "hybernateToolStripMenuItem";
+            this.hybernateToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.hybernateToolStripMenuItem.Text = "Hybernate";
+            this.hybernateToolStripMenuItem.Click += new System.EventHandler(this.hybernateToolStripMenuItem_Click);
+            // 
+            // logOFFToolStripMenuItem
+            // 
+            this.logOFFToolStripMenuItem.Name = "logOFFToolStripMenuItem";
+            this.logOFFToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.logOFFToolStripMenuItem.Text = "Log OFF";
+            this.logOFFToolStripMenuItem.Click += new System.EventHandler(this.logOFFToolStripMenuItem_Click);
+            // 
+            // lockUserToolStripMenuItem
+            // 
+            this.lockUserToolStripMenuItem.Name = "lockUserToolStripMenuItem";
+            this.lockUserToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.lockUserToolStripMenuItem.Text = "Lock User";
+            this.lockUserToolStripMenuItem.Click += new System.EventHandler(this.lockUserToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGreen;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 57);
+            this.button1.TabIndex = 1;
+            this.button1.TabStop = false;
+            this.button1.Tag = "power";
+            this.button1.Text = "Power OFF";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGreen;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(12, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 57);
+            this.button2.TabIndex = 2;
+            this.button2.TabStop = false;
+            this.button2.Tag = "restart";
+            this.button2.Text = "Restart System";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGreen;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(12, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(190, 57);
+            this.button3.TabIndex = 3;
+            this.button3.TabStop = false;
+            this.button3.Tag = "sleep";
+            this.button3.Text = "Sleep";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightGreen;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(208, 75);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(190, 57);
+            this.button4.TabIndex = 5;
+            this.button4.TabStop = false;
+            this.button4.Tag = "lock";
+            this.button4.Text = "Lock User";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightGreen;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(208, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(190, 57);
+            this.button5.TabIndex = 4;
+            this.button5.TabStop = false;
+            this.button5.Tag = "logout";
+            this.button5.Text = "User Log Out";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LightGreen;
+            this.button6.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button6.Location = new System.Drawing.Point(209, 138);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(190, 57);
+            this.button6.TabIndex = 6;
+            this.button6.TabStop = false;
+            this.button6.Tag = "hibernate";
+            this.button6.Text = "Hibernate";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(411, 205);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Power Control";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem powerOFFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sleepToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hybernateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logOFFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lockUserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ PowerControl/MainForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerControl
+{
+    public partial class MainForm : Form
+    {
+        private bool allowExit = false;
+
+        [DllImport("DemoCLibrary.dll")]
+        static extern void LockUser();
+
+        [DllImport("DemoCLibrary.dll")]
+        static extern void PowerOff();
+
+        [DllImport("DemoCLibrary.dll")]
+        static extern void UserLogOut();
+
+        [DllImport("DemoCLibrary.dll")]
+        static extern void RestartSystem();
+
+        [DllImport("DemoCLibrary.dll")]
+        static extern void SeepSystem();
+
+        [DllImport("DemoCLibrary.dll")]
+        static extern void HibernateSystem();
+
+
+
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            notifyIcon1.Visible = false;
+            this.notifyIcon1.MouseDoubleClick += notifyIcon1_MouseDoubleClick;
+            this.Resize += this.MainForm_Resize;
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            var butt = (sender as Button);
+            switch (butt.Tag.ToString())
+            {
+                case "power":
+                        PowerOff();
+                        break;
+                case "restart":
+                    RestartSystem();
+                    break;
+                case "sleep":
+                    SeepSystem();
+                    break;
+                case "hibernate":
+                    HibernateSystem();
+                    break;
+                case "lock":
+                    LockUser();
+                    break;
+                case "logout":
+                    UserLogOut();
+                    break;
+            }
+        }
+
+        private void powerOFFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PowerOff();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            allowExit = true;
+            Application.Exit();
+        }
+
+        private void sleepToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SeepSystem();
+        }
+
+        private void hybernateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HibernateSystem();
+        }
+
+        private void logOFFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UserLogOut();
+        }
+
+        private void lockUserToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LockUser();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+
+            if (allowExit)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+
+        }
+    }
+}
